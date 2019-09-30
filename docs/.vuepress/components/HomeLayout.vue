4ddce9f80428fc23a5419279115bde27dfe5c384
Vikram Rangnekar <vicram@gmail.com>
2019-09-30 01:25:23 -0400

    Update documentation site

--- docs/.vuepress/components/HomeLayout.vue
+++ docs/.vuepress/components/HomeLayout.vue
@@ -1,64 +1,80 @@
 <template>
-  <div class="container mx-auto" style="background-color: #f3f9fd;">
-  <div class="mt-16">
-  <main aria-labelledby="main-title" >
-    <Navbar
-      @toggle-sidebar="toggleSidebar"
-    />
+  <div>
+    <main aria-labelledby="main-title" >
+      <Navbar
+        @toggle-sidebar="toggleSidebar"
+      />
 
-    <div>
-      <div class="p-4 pt-12 border-white border-b-4">
-        <div class="text-center">
-          <h1 v-if="data.heroText !== null" class="text-5xl">{{ data.heroText || $title || 'Hello' }}</h1>
-          
-          <p class="text-2xl text-gray-600 leading-tight mt-4 xl:mt-0">
-            {{ data.tagline || $description || 'Welcome to your VuePress site' }}
-          </p>
+    <div class="container mx-auto">
+      <div class="flex flex-col md:flex-row justify-between px-10 md:px-20">
+        <div class="bg-bottom bg-no-repeat bg-cover">
+          <div class="text-center md:text-left pt-24">
+            <h1 v-if="data.heroText !== null" class="text-5xl font-bold text-black pb-0 uppercase">
+              {{ data.heroText || $title || 'Hello' }}
+            </h1>
+            
+            <p class="text-2xl text-gray-700 leading-tight pb-0">
+              {{ data.tagline || $description || 'Welcome to your VuePress site' }}
+            </p>
 
-          <NavLink
-            class="inline-block px-4 py-2 my-8 bg-green-500 text-green-100 font-bold rounded"
-            :item="actionLink"
-          />
+            <p class="text-lg text-gray-600 leading-tight">
+              {{ data.longTagline }}
+            </p>
+
+            <NavLink
+              class="inline-block px-4 py-3 my-8 bg-blue-600 text-blue-100 font-bold rounded"
+              :item="actionLink"
+            />
+          </div>
         </div>
 
+        <div class="py-10 md:p-20">
+          <img src="/hologram.svg" class="h-64">
+        </div>
+
+      </div>
+      </div>
+
+      <div>
         <div
           class="flex flex-wrap mx-2 md:mx-20"
           v-if="data.features && data.features.length"
         >
           <div
-            class="w-2/4 md:w-1/3"
+            class="w-2/4 md:w-1/3 shadow"
             v-for="(feature, index) in data.features"
             :key="index"
           >
-            <div class="p-4 pl-0 pb-8">
-            <h2 class="md:text-2xl font-medium border-0 mb-2">{{ feature.title }}</h2>
-            <p class="md:text-xl text-gray-600 leading-snug">{{ feature.details }}</p>
+            <div class="p-8">
+            <h2 class="md:text-xl text-blue-800 font-medium border-0 mb-1">{{ feature.title }}</h2>
+            <p class="md:text-xl text-gray-700 leading-snug">{{ feature.details }}</p>
             </div>
           </div>
         </div>
       </div>
 
-      <div class="border-white border-b-4">
-       <img
-          v-if="data.heroImage"
-          :src="$withBase(data.heroImageMobile)"
-          :alt="data.heroAlt || 'hero'"
-        >
+      <div class="bg-gray-100 my-10">
+        <div class="container mx-auto text-center px-10 md:px-0 py-32">
+          <h1 class="uppercase font-semibold text-xl text-blue-800 mb-10">
+            What is {{ data.heroText }}?
+          </h1>
+          <div class="text-2xl md:text-3xl">
+            {{ data.description }}
+          </div>
+        </div>
       </div>
-    </div>
 
-    <div class="theme-default-content markdown">
-      <Content />
-    </div>
-      
-    <div
-      class="mx-auto text-center py-8"
-      v-if="data.footer"
-    >
-      {{ data.footer }}
-    </div>
-  </main>
-  </div>
+      <div class="theme-default-content markdown">
+        <Content />
+      </div>
+        
+      <div
+        class="mx-auto text-center py-8"
+        v-if="data.footer"
+      >
+        {{ data.footer }}
+      </div>
+    </main>
   </div>
 </template>
 
--- docs/README.md
+++ docs/README.md
@@ -2,12 +2,16 @@
 layout: HomeLayout
 
 home: true
-heroText: "SUPER GRAPH"
+heroText: "Super Graph"
 heroImage: /super-graph-web-ui-half.png
 heroImageMobile: /super-graph-web-ui.png
-tagline: An instant high-performance GraphQL API. No code needed.
-actionText: Get Started →
+tagline: Build web products faster. Instant APIs for your apps
+longTagline: Get an instant high performance GraphQL API for Postgres. No code needed. GraphQL is automatically transformed into efficient database queries.
+actionText: Get Started, Free, Open Source →
 actionLink: /guide
+
+description: Super Graph can automatically learn a Postgres database and instantly serve it as a fast and secured GraphQL API. It comes with tools to create a new app and manage it's database. You get it all, a very productive developer and a highly scalable app backend. It's designed to work well on serverless platforms by Google, AWS, Microsoft, etc. The goal is to save you a ton of time and money so you can focus on you're apps core value.
+
 features:
 - title: Simple
   details: Easy config file, quick to deploy, No code needed. It just works.
@@ -16,11 +20,12 @@ features:
 - title: Ruby-on-Rails
   details: Can read Rails cookies and supports rails database conventions.
 - title: Serverless
-  details: Designed for App Engine, Kubernetes, CloudRun, Heroku, AWS Fargate, etc  
+  details: Instant startup for scale to zero environments like Google Cloud Run, App Engine, AWS Lambda 
 - title: Go Lang
   details: Go is a language created at Google to build fast and secure web services. 
 - title: Free and Open Source
   details: Not a VC funded startup. Not even a startup just good old open source code
+
 footer: MIT Licensed | Copyright © 2018-present Vikram Rangnekar
 ---
 
