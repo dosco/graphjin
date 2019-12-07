b9bfb90f1901d92286840b8bad1327faf1314f3f
Захаров Лев Юрьевич <l.j.zakharov@gmail.com>
2019-12-07 11:53:13 +0300

    Add healthy response (#19)

--- serv/health.go
+++ serv/health.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 )
 
+var healthyResponse = []byte("All's Well")
+
 func health(w http.ResponseWriter, _ *http.Request) {
 	conn, err := db.Acquire(context.Background())
 	if err != nil {
@@ -16,6 +18,12 @@ func health(w http.ResponseWriter, _ *http.Request) {
 	ctx, _ := context.WithTimeout(context.Background(), conf.DB.PingTimeout)
 	if err := conn.Conn().Ping(ctx); err != nil {
 		errlog.Error().Err(err).Msg("error pinging database")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := w.Write(healthyResponse); err != nil {
+		errlog.Error().Err(err).Msg("error writing healthy response")
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
