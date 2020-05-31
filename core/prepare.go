816121fbcf3aa5bb5cb7a5ce1b288874403c0808
Vikram Rangnekar <vicram@gmail.com>
2020-05-31 18:10:43 -0400

    fix: issue with early return in prepare statement function

--- core/prepare.go
+++ core/prepare.go
@@ -56,7 +56,7 @@ func (sg *SuperGraph) initPrepared() error {
 
 		err := sg.prepareStmt(v)
 		if err != nil {
-			sg.log.Printf("WRN %s: %v", v.Name, err)
+			return err
 		} else {
 			success++
 		}
@@ -74,6 +74,7 @@ func (sg *SuperGraph) prepareStmt(item allow.Item) error {
 
 	qt := qcode.GetQType(query)
 	ct := context.Background()
+
 	switch qt {
 	case qcode.QTQuery:
 		var stmts1 []stmt
@@ -85,43 +86,36 @@ func (sg *SuperGraph) prepareStmt(item allow.Item) error {
 			stmts1, err = sg.buildRoleStmt(qb, vars, "user")
 		}
 
-		if err != nil {
-			return err
-		}
-
-		//logger.Debug().Msgf("Prepared statement 'query %s' (user)", item.Name)
-
-		err = sg.prepare(ct, stmts1, stmtHash(item.Name, "user"))
-		if err != nil {
-			return err
+		if err == nil {
+			if err = sg.prepare(ct, stmts1, stmtHash(item.Name, "user")); err != nil {
+				return err
+			}
+		} else {
+			sg.log.Printf("WRN query %s: %v", item.Name, err)
 		}
 
 		if sg.anonExists {
-			// logger.Debug().Msgf("Prepared statement 'query %s' (anon)", item.Name)
-
 			stmts2, err := sg.buildRoleStmt(qb, vars, "anon")
-			if err != nil {
-				return err
-			}
 
-			err = sg.prepare(ct, stmts2, stmtHash(item.Name, "anon"))
-			if err != nil {
-				return err
+			if err == nil {
+				if err = sg.prepare(ct, stmts2, stmtHash(item.Name, "anon")); err != nil {
+					return err
+				}
+			} else {
+				sg.log.Printf("WRN query %s: %v", item.Name, err)
 			}
 		}
 
 	case qcode.QTMutation:
 		for _, role := range sg.conf.Roles {
-			// logger.Debug().Msgf("Prepared statement 'mutation %s' (%s)", item.Name, role.Name)
-
 			stmts, err := sg.buildRoleStmt(qb, vars, role.Name)
-			if err != nil {
-				return err
-			}
 
-			err = sg.prepare(ct, stmts, stmtHash(item.Name, role.Name))
-			if err != nil {
-				return err
+			if err == nil {
+				if err = sg.prepare(ct, stmts, stmtHash(item.Name, role.Name)); err != nil {
+					return err
+				}
+			} else {
+				sg.log.Printf("WRN mutation %s: %v", item.Name, err)
 			}
 		}
 	}
