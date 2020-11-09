8e1db409d3000f6b6d21cb4c640e7d5164103e59
Vikram Rangnekar <vicram@gmail.com>
2020-11-09 00:19:34 -0500

    fix: broken tests

--- core/internal/graph/parse_test.go
+++ core/internal/graph/parse_test.go
@@ -54,7 +54,7 @@ func BenchmarkParse(b *testing.B) {
 	b.ResetTimer()
 	b.ReportAllocs()
 	for n := 0; n < b.N; n++ {
-		_, err := Parse(gql)
+		_, err := Parse(gql, nil)
 
 		if err != nil {
 			b.Fatal(err)
@@ -68,7 +68,7 @@ func BenchmarkParseP(b *testing.B) {
 
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			_, err := Parse(gql)
+			_, err := Parse(gql, nil)
 
 			if err != nil {
 				b.Fatal(err)
@@ -81,7 +81,7 @@ func BenchmarkParseFragment(b *testing.B) {
 	b.ResetTimer()
 	b.ReportAllocs()
 	for n := 0; n < b.N; n++ {
-		_, err := Parse(gqlWithFragments)
+		_, err := Parse(gqlWithFragments, nil)
 
 		if err != nil {
 			b.Fatal(err)
