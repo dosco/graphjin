193721a7d32fe0923bb34767912975415d00b50b
Diego Szychowski <diegos@exo.com.ar>
2021-09-08 16:47:37 -0300

    fix missing userID in scripts and also an intermittent panic (#273)
    * Fix: allow enable TLS with just server cert
    
    When enabling TLS support, only the server certificate is required. The
    client certificate and key are optional. Sometimes you would like to enable
    TLS for the database but without using a client verification. You only want
    TLS and only validate the server using a CA certificate. Another small change
    is that you could also specify the server and client certificates and the
    client key defining the PEM in one line.
    
    * fix missing userID  in scripts request func
    
    * fix intermitent panic when using scripts

--- core/script.go
+++ core/script.go
@@ -77,7 +77,7 @@ func (c *gcontext) scriptCallReq(vars []byte, role string) (_ []byte, err error)
 	}
 
 	var userID interface{}
-	if v := c.Value(UserIDKey); v == nil {
+	if v := c.Value(UserIDKey); v != nil {
 		userID = v
 	}
 
@@ -111,7 +111,7 @@ func (c *gcontext) scriptCallResp(data []byte, role string) (_ []byte, err error
 	}
 
 	var userID interface{}
-	if v := c.Value(UserIDKey); v == nil {
+	if v := c.Value(UserIDKey); v != nil {
 		userID = v
 	}
 
@@ -162,7 +162,7 @@ func (c *gcontext) scriptInit(s *script, name string) error {
 
 	if s.vm != nil {
 		s.vm.ClearInterrupt()
-
+		vm = s.vm
 	} else {
 		vm = goja.New()
 
