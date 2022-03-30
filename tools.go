099c0b5ab133af71c21cf19a104001e949474184
Vikram Rangnekar <832235+dosco@users.noreply.github.com>
2022-03-30 02:25:43 -0700

    Remove goreleaser from tools.go requires go1.18

--- tools.go
+++ tools.go
@@ -6,7 +6,6 @@ package main
 import (
 	_ "github.com/git-chglog/git-chglog/cmd/git-chglog"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/goreleaser/goreleaser"
 	_ "golang.org/x/perf/cmd/benchstat"
 	_ "golang.org/x/tools/cmd/stringer"
 )
