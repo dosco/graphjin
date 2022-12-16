149b7aa97a6a63422e1d9a5f2eb796068216d38f
dosco <832235+dosco@users.noreply.github.com>
2022-12-16 00:59:27 -0800

    fix: missing files in wasm example

--- wasm/args.go
+++ wasm/args.go
@@ -15,8 +15,8 @@ type queryArgs struct {
 }
 
 func newQueryArgs(args []js.Value) (qa queryArgs, jsErr js.Value) {
-	if len(args) < 2 {
-		err := errors.New("required arguments: query, variables")
+	if len(args) < 1 {
+		err := errors.New("required arguments: query")
 		return qa, toJSError(err)
 	}
 	query := args[0]
@@ -24,14 +24,14 @@ func newQueryArgs(args []js.Value) (qa queryArgs, jsErr js.Value) {
 	if query.Type() != js.TypeString || query.String() == "" {
 		return qa, toJSError(errors.New("query argument missing"))
 	}
-
 	qa.query = query.String()
+
 	return processQueryArgs(qa, args)
 }
 
 func newQueryByNameArgs(args []js.Value) (qa queryArgs, jsErr js.Value) {
-	if len(args) < 2 {
-		err := errors.New("required arguments: name, variables")
+	if len(args) < 1 {
+		err := errors.New("required arguments: name")
 		return qa, toJSError(err)
 	}
 	name := args[0]
@@ -39,33 +39,39 @@ func newQueryByNameArgs(args []js.Value) (qa queryArgs, jsErr js.Value) {
 	if name.Type() != js.TypeString || name.String() == "" {
 		return qa, toJSError(errors.New("query argument missing"))
 	}
-
 	qa.query = name.String()
+
 	return processQueryArgs(qa, args)
 }
 
 func processQueryArgs(qa queryArgs, args []js.Value) (queryArgs, js.Value) {
-	var err error
+	if len(args) == 1 {
+		return qa, js.Null()
+	}
 	vars := args[1]
-	opts := args[2]
 
 	if vars.Type() != js.TypeObject &&
-		vars.Type() != js.TypeNull && vars.Type() != js.TypeUndefined {
-		err = errors.New("variables argument can only be a string or null")
-	}
-
-	if opts.Type() != js.TypeObject &&
-		opts.Type() != js.TypeNull && opts.Type() != js.TypeUndefined {
-		err = errors.New("options argument can only be a object or null")
-	}
-
-	if err != nil {
+		vars.Type() != js.TypeNull &&
+		vars.Type() != js.TypeUndefined {
+		err := errors.New("variables argument can only be a string or null")
 		return qa, toJSError(err)
 	}
 
 	if vars.Type() == js.TypeObject {
 		val := js.Global().Get("JSON").Call("stringify", vars)
 		qa.vars = json.RawMessage(val.String())
+	}
+
+	if len(args) == 2 {
+		return qa, js.Null()
+	}
+	opts := args[2]
+
+	if opts.Type() != js.TypeObject &&
+		opts.Type() != js.TypeNull &&
+		opts.Type() != js.TypeUndefined {
+		err := errors.New("options argument can only be a object or null")
+		return qa, toJSError(err)
 	}
 
 	if v := opts.Get("userID"); v.Type() == js.TypeString || v.Type() == js.TypeNumber {
